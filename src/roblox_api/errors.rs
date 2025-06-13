88405dad67c0f628fe89f7089b5024eee051ed4f
Brandon <badamsva@gmail.com>
2025-06-13 01:11:19 -0400

    define simple request errors

---
+++ src/roblox_api/errors.rs
@@ -0,0 +1,22 @@
+use thiserror::Error;
+
+#[non_exhaustive]
+#[derive(Error, Debug, Default)]
+pub enum RobloxError {
+    #[default]
+    #[error("Roblosecurity Not Set")]
+    RoblosecurityNotSet,
+
+    #[error("Unknown Roblox Error Code {code}: {message}")]
+    UnknownRobloxErrorCode { code: u16, message: String },
+    #[error("Invalid Xcsrf. New Xcsrf Contained In Error.")]
+    InvalidXcsrf(String),
+
+    #[error("Missing Xcsrf")]
+    XcsrfNotReturned,
+
+    #[error(
+        "Unknown Status Code 403 Format. If this occurs often it may be a bug. Please report it to the issues page."
+    )]
+    UnknownStatus403Format,
+}
