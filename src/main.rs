4729ff88b7cdb5f20bc7e70569a4ef9b034fba72
Brandon <badamsva@gmail.com>
2025-06-14 23:34:12 -0400

    pull animation as a test

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,14 @@ async fn main() {
         .build()
         .expect("Failed to build Roblox session");
 
-    match session.pull_animation(123141414).await {
-        Ok(body) => println!("{}", body),
+    let result = session.refresh_csrf().await;
+    match result {
+        Ok(test) => println!("Refreshed XCSRF {:?}", test),
+        Err(e) => eprintln!("Failed to refresh xCSRF {}", e),
+    }
+
+    match session.pull_animation(128511411359897).await {
+        Ok(body) => println!("hi"),
         Err(e) => eprintln!("Failed to fetch animation: {}", e),
     }
 }
