62dd4d1e1e13d6be2467203154ba2f670d9be33f
Brandon <badamsva@gmail.com>
2025-06-15 18:39:06 -0400

    test the publishing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use animation_replace_roblox::roblox_api::assetdelivery::request_types::Animation;
 use animation_replace_roblox::roblox_api::roblox_client::SessionBuilder;
 use dotenv::dotenv;
 
@@ -11,14 +12,30 @@ async fn main() {
         .build()
         .expect("Failed to build Roblox session");
 
-    let result = session.refresh_csrf().await;
-    match result {
-        Ok(test) => println!("Refreshed XCSRF {:?}", test),
-        Err(e) => eprintln!("Failed to refresh xCSRF {}", e),
-    }
+    // let result = session.refresh_csrf().await;
+    // match result {
+    //     Ok(test) => println!("Refreshed XCSRF {:?}", test),
+    //     Err(e) => eprintln!("Failed to refresh xCSRF {}", e),
+    // }
 
     match session.pull_animation(128511411359897).await {
-        Ok(body) => println!("hi"),
+        Ok(animation_data) => {
+            let new_animation = Animation {
+                id: None,
+                title: String::from("Test"),
+                description: String::from("test"),
+                group_id: None,
+                animation_data: Some(String::from(animation_data)),
+            };
+
+            match session.publish_animation(new_animation).await {
+                Ok(_) => {}
+                Err(e) => {
+                    // Handle the error if necessary
+                    eprintln!("Error: {:?}", e);
+                }
+            }
+        } // Do nothing here
         Err(e) => eprintln!("Failed to fetch animation: {}", e),
     }
 }
