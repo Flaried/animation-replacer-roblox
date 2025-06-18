ae4892ec3c436e331fe00e37787be1beb227ef39
Brandon <badamsva@gmail.com>
2025-06-18 00:23:49 -0400

    wip: studio parser

---
+++ src/studio/parser.rs
@@ -0,0 +1,6 @@
+use rbx_binary::{from_reader, to_writer};
+use rbx_dom_weak::{InstanceBuilder, WeakDom};
+use std::fs::File;
+
+pub fn find_animations() -> Result<(), Box<dyn std::error::Error>> {}
+
