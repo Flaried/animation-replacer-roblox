ae3091c5df869db82941d6a2a580fe364f564977
Brandon <badamsva@gmail.com>
2025-06-19 00:48:30 -0400

    feat: make a parser that gets all animation objects in .rbxl

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,14 @@ pub mod animation_uploader;
 
 /// A module for handling parsing and editing studio files
 pub mod studio;
+
+use rbx_dom_weak::{Instance, WeakDom};
+
+pub struct Animation<'a> {
+    pub instance: &'a Instance,
+    pub animation_id: String,
+}
+
+pub struct StudioParser {
+    pub dom: WeakDom,
+}
--- src/studio/parser.rs
+++ src/studio/parser.rs
@@ -1,6 +1,36 @@
-use rbx_binary::{from_reader, to_writer};
-use rbx_dom_weak::{InstanceBuilder, WeakDom};
+use crate::{Animation, StudioParser};
+use rbx_binary::from_reader;
+use rbx_types::Variant;
 use std::fs::File;
+use ustr::Ustr;
 
-pub fn find_animations() -> Result<(), Box<dyn std::error::Error>> {}
+impl StudioParser {
+    pub fn workspace_animations(&self) -> Vec<Animation> {
+        let mut animations = Vec::new();
+
+        for instance in self.dom.descendants() {
+            if instance.class == "Animation" {
+                if let Some(instance_type) = instance.properties.get(&Ustr::from("AnimationId")) {
+                    if let Variant::ContentId(content_id) = instance_type {
+                        let url = content_id.as_str();
+                        if !url.is_empty() {
+                            animations.push(Animation {
+                                instance,
+                                animation_id: url.to_string(),
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        animations
+    }
+
+    pub fn from_rbxl(file_name: String) -> Result<StudioParser, Box<dyn std::error::Error>> {
+        let file = File::open(file_name)?;
+        let dom = from_reader(file)?;
+
+        Ok(StudioParser { dom })
+    }
+}
 
