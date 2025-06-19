b7d71a33fc0e7b4282242892bcc03af7404e0ce0
Brandon <badamsva@gmail.com>
2025-06-19 00:48:40 -0400

    test the studio parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use animation_replace_roblox::animation_uploader;
+use animation_replace_roblox::{StudioParser, animation_uploader};
 use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let parser = match StudioParser::from_rbxl("test.rbxl".to_string()) {
+        Ok(parser) => parser,
+        Err(e) => {
+            eprintln!("Error loading file: {}", e);
+            return;
+        }
+    };
+
+    parser.workspace_animations();
 
     match animation_uploader::reupload_animation().await {
         Ok(_) => {}
