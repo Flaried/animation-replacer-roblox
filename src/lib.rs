923a8c040ef96454f241b6048892805d010ecc93
Brandon <badamsva@gmail.com>
2025-06-19 16:23:55 -0400

    rename animation_uploader to animation

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,33 @@
+use rbx_dom_weak::{Instance, WeakDom};
+
 /// A module for uploading animations
-pub mod animation_uploader;
+pub mod animation;
 
 /// A module for handling parsing and editing studio files
 pub mod studio;
 
-use rbx_dom_weak::{Instance, WeakDom};
-
+/// A structure to store the Instance and Id of the animation
 pub struct Animation<'a> {
     pub instance: &'a Instance,
     pub animation_id: String,
 }
 
+/// A structure to be impl for the roblox studio DOM
 pub struct StudioParser {
     pub dom: WeakDom,
+}
+
+// A structure to store the instance, source code and class name of a script
+pub struct Script<'a> {
+    pub instance: &'a Instance,
+    pub source: String,
+    pub script_type: ScriptType,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ScriptType {
+    Script,
+    LocalScript,
+    ModuleScript,
+    Unknown(String), // fallback for non-standard classes
 }
