7be8ace4bf2cf6e67a67d645f002c8eacbe5cb22
Brandon <badamsva@gmail.com>
2025-06-29 23:26:20 -0400

    test the new workflow of getting animations.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use animation_replace_roblox::{StudioParser, animation};
+use animation_replace_roblox::StudioParser;
 use dotenv::dotenv;
+// use log::{debug, info};
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     dotenv().ok();
 
     let file_path = shellexpand::tilde("~/Documents/Place1.rbxl");
-
     let roblox_cookie = std::env::var("ROBLOSECURITY").expect(".ROBLOSECURITY must be set in .env");
 
     // Build the parser with the roboat client
-    let parser = match StudioParser::builder()
+    let mut parser = match StudioParser::builder()
         .file_path(file_path.as_ref())
         .roblosecurity(roblox_cookie)
         .build()
@@ -22,27 +23,24 @@ async fn main() {
         }
     };
 
-    let animations = parser.workspace_animations();
-
-    let scripts = parser.all_scripts();
-    for script in scripts {
-        match parser.animations_in_script(&script).await {
-            Ok(animations) => {
-                println!("{:?}", animations);
-            }
-            Err(e) => {
-                eprintln!("Failed to get workspace animations: {:?}", e);
-            }
-        };
-        // for animation_id in script_animations {
-        //     println!("{:?}", animation_id);
-        // }
+    let workspace_animations = parser.workspace_animations();
+    match workspace_animations.await {
+        Ok(animations) => {
+            println!("Animations: {:?}", animations);
+        }
+        Err(e) => {
+            eprintln!("Failed to fetch animations: {:?}", e);
+        }
     }
 
-    // match parser.reupload_animation().await {
-    //     Ok(_) => {}
-    //     Err(e) => {
-    //         eprintln!("Failed uploading animation {:?}", e)
-    //     }
-    // }
+    let script_animations = parser.all_animations_in_scripts();
+
+    match script_animations.await {
+        Ok(animations) => {
+            println!("Animations: {:?}", animations);
+        }
+        Err(e) => {
+            eprintln!("Failed to fetch animations: {:?}", e);
+        }
+    }
 }
