07dca18e279ca95aae9e6995fa52d22dc4aff6f0
Brandon <badamsva@gmail.com>
2025-06-29 23:26:04 -0400

    feature: add  debug, clone to animation and add simple methods.
    I added new and from maybe for the future..

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,38 @@ pub mod animation;
 /// A module for handling parsing and editing studio files
 pub mod studio;
 
+/// A module for handling parsing and editing on scripts, in studio files.
+pub mod script;
+
 /// A structure to store the Instance and Id of the animation
+#[derive(Debug, Clone)]
 pub struct Animation<'a> {
     pub instance: &'a Instance,
     pub animation_id: String,
 }
 
+impl<'a> Animation<'a> {
+    pub fn new(instance: &'a Instance, animation_id: String) -> Self {
+        Self {
+            instance,
+            animation_id,
+        }
+    }
+
+    pub fn with_info(instance: &'a Instance, animation_id: String) -> Self {
+        Self {
+            instance,
+            animation_id,
+        }
+    }
+}
 /// A structure to be impl for the roblox studio DOM
 pub struct StudioParser {
     pub roblosecurity: Option<String>,
     pub dom: WeakDom,
 }
 
-// A structure to store the instance, source code and class name of a script
+/// A structure to store the instance, source code and class name of a script
 pub struct Script<'a> {
     pub instance: &'a Instance,
     pub source: String,
