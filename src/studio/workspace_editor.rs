b6c033d0c76546e24c009a957b62daaeb483d995
Brandon <badamsva@gmail.com>
2025-06-30 22:13:49 -0400

    bug fix: writing the root's children instead of the root, avoiding corrupted files.

--- src/studio/workspace_editor.rs
+++ src/studio/workspace_editor.rs
@@ -24,7 +24,11 @@ impl StudioParser {
     ) -> Result<(), Box<dyn std::error::Error>> {
         let expanded_path = shellexpand::full(file_path.as_ref().to_str().unwrap())?;
         let file = File::create(expanded_path.as_ref())?;
-        to_writer(file, &self.dom, &[self.dom.root_ref()])?;
+
+        // Get the children of the root instead of the root
+        let root_children = self.dom.get_by_ref(self.dom.root_ref()).unwrap().children();
+
+        to_writer(file, &self.dom, root_children)?;
         Ok(())
     }
 }
