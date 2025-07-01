ab661644a28e15ec16c0bf253291f3f13c0d545b
Brandon <badamsva@gmail.com>
2025-06-30 22:24:02 -0400

    rename: workspace_editor to file_editor

--- src/studio/file_editor.rs
+++ src/studio/file_editor.rs
@@ -1,11 +1,7 @@
+use crate::StudioParser;
 use rbx_binary::to_writer;
-use rbx_dom_weak::Instance;
-use rbx_types::Variant;
 use std::fs::File;
 use std::path::Path;
-use ustr::Ustr;
-
-use crate::StudioParser;
 
 impl StudioParser {
     /// Saves the current DOM state to a .rbxl file
